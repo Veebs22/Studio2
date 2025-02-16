af15ae0c305b7b21783af1693e6fa9f3a4b66bb7
Veebs22 <97552796+Veebs22@users.noreply.github.com>
2025-02-15 16:51:22 -0800

    halfway done! added prefabs for pins, gutters, and player
    halfway done! added prefabs for pins, gutters, and player

---
+++ Assets/Gutter.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Gutter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider triggeredBody)
+    {
+        Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
+        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
+        ballRigidBody.linearVelocity = Vector3.zero;
+        ballRigidBody.AddForce(transform.up * velocityMagnitude, ForceMode.VelocityChange);
+
+
+
+    }
+
+
+}
